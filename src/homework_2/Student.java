5c829642c3f00410758916064d7c0206a7fcbaf7
joeyg <joeyg@Joey_Desktop>
2024-02-06 12:52:31 -0600

    Created the Main Class
    Created Student Class
    
    Added Set and Get to Student Class
    Created Private Variables Id, Name, Grade in Student Class

---
+++ src/homework_2/Student.java
@@ -0,0 +1,50 @@
+package homework_2;
+
+public class Student {//Create a student class with private of the following fields: int id, String name, and double grade
+	private int id;
+	private String name;
+	private double grade;
+
+	public Student() {
+		id = 0;
+		name = "";
+		grade = 0.0;
+	}
+
+	public Student(int Id, String Name, double Grade){
+		this.id = Id;
+		this.name = Name;
+		this.grade = Grade;
+	}
+	
+	//Initialize the Set methods
+	public void setId(int ID) {
+		this.id = ID;
+	}
+	public void setName(String Name) {
+		this.name = Name;
+	}
+	public void setGrade(double Grade) {
+		this.grade = Grade;
+	}
+	
+	//Initialize the Get methods
+	public int getId() {
+		return this.id;
+	}
+	public String getName() {
+		return this.name;
+	}
+	public double getGrade() {
+		return this.grade;
+	}
+	
+	//Initialize a toString method to print out the student information
+	public String toString() {
+		String StringToPrint = "The Student has the id of: " + this.id + 
+				"\nThe Student has the Name of: " + this.name + 
+				"\nThe Student has the Grade of: " + this.grade;
+		return StringToPrint;
+	}
+	
+}
--- src/module-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * 
- */
-/**
- * 
- */
-module Group_S_Homework_2 {
-}
